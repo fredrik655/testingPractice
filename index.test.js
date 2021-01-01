95614076f7fbf194dd33b24b9a97f8b800b539d1
anon <anon@example.com>
2021-01-01 10:49:43 +0100

    Update test to use capitalize func

--- index.test.js
+++ index.test.js
@@ -1,8 +1,8 @@
 const { test, expect } = require("@jest/globals");
-const {capitalize} = require('./index');
+const capitalize = require('./index');
 
 test('capitalize first letter of string ', () => {
-  expect('Hello').toBe('Hello');
-  expect('abc').toBe('Abc');
-  expect('capitalize').toBe('Capitalize');
+  expect(capitalize('Hello')).toBe('Hello');
+  expect(capitalize('abc')).toBe('Abc');
+  expect(capitalize('capitalize')).toBe('Capitalize');
 })
