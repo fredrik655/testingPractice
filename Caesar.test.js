496e62b965f9456a5e6ae641d1794b1546ed4cbc
anon <anon@example.com>
2021-01-01 13:38:47 +0100

    Implemented caesar cipher test cases

---
+++ Caesar.test.js
@@ -0,0 +1,37 @@
+const { test, expect } = require('@jest/globals');
+const caesar = require('./Caesar');
+
+
+test('shift with index', () => {
+  expect(caesar('abc',1)).toBe('bcd');
+  expect(caesar('hello',5)).toBe('mjqqt');
+  expect(caesar('caesar',3)).toBe('fdhvdu');
+})
+
+test('shift with neg index', () => {
+  expect(caesar('abc',-1)).toBe('zab');
+  expect(caesar('hello',-2)).toBe('fcjjm');
+  expect(caesar('caesar',-2)).toBe('aycqyp');
+})
+
+test('shift with wrapping lowercase', () => {
+  expect(caesar('xyz',3)).toBe('abc');
+  expect(caesar('hello',14)).toBe('vszzc');
+  expect(caesar('caesar',16)).toBe('squiqh');
+})
+
+test('shift with wrapping uppercase', () => {
+  expect(caesar('XYZ',3)).toBe('ABC');
+  expect(caesar('HELLO',14)).toBe('VSZZC');
+  expect(caesar('CAESAR',16)).toBe('SQUIQH');
+})
+
+test('shift with upper and lowercase', () => {
+  expect(caesar('Xzy',3)).toBe('Abc');
+  expect(caesar('hEllo',14)).toBe('vSzzc');
+  expect(caesar('caEsaR',16)).toBe('sqUiqH');
+})
+
+test('shift with punctuation', () => {
+  expect(caesar('hello world.',8)).toBe('pmttw ewztl.');
+})
