0ee0375911290b51e217110ff7cd9b112b00d67a
anon <anon@example.com>
2021-01-01 11:32:57 +0100

    implement calculator add func

---
+++ Calculator.js
@@ -0,0 +1,18 @@
+
+const add = (a ,b) => {
+  return (a + b);
+}
+
+const subtract = (a, b) => {
+
+}
+
+const divide = (a, b) => {
+
+}
+
+const multiply = (a, b) => {
+
+}
+
+module.exports = {add , subtract, divide, multiply};
