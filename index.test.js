34c163cb74e9ee44431f2c1fcc25f9cbd6f7c9cd
anon <anon@example.com>
2021-01-01 11:18:09 +0100

    Made test case for reveseString func

--- index.test.js
+++ index.test.js
@@ -1,9 +1,18 @@
 const { test, expect } = require("@jest/globals");
-const capitalize = require('./index');
+const indexModule = require('./index');
+const capitalize = indexModule.capitalize;
+const reverseString = indexModule.reverseString;
+
 
 test('capitalize first letter of string ', () => {
   expect(capitalize('Hello')).toBe('Hello');
   expect(capitalize('abc')).toBe('Abc');
   expect(capitalize('capitalize')).toBe('Capitalize');
   expect(capitalize('hello WorLd')).toBe('Hello world');
+})
+
+test('Reverses a string', () => {
+  expect(reverseString('Hello World')).toBe('dlroW olleH');
+  expect(reverseString('Abcd')).toBe('dcbA');
+  expect(reverseString('Anna')).toBe('annA');
 })
