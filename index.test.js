d646b4a10f33d52106a3b9c40055e152f45b96ed
anon <anon@example.com>
2021-01-01 11:11:57 +0100

    Added new test cases

--- index.test.js
+++ index.test.js
@@ -5,4 +5,5 @@ test('capitalize first letter of string ', () => {
   expect(capitalize('Hello')).toBe('Hello');
   expect(capitalize('abc')).toBe('Abc');
   expect(capitalize('capitalize')).toBe('Capitalize');
+  expect(capitalize('hello WorLd')).toBe('Hello world');
 })
