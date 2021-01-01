48be98be6275e8a1f6b9a968cc72e1da58295b17
anon <anon@example.com>
2021-01-01 11:22:37 +0100

    Implement reverseString func

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const { expect } = require("@jest/globals");
 
 
 const capitalize = (string) => {
-  
-
   return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
 
-module.exports = capitalize;
+const reverseString = string => {
+  const stringArr = Array.from(string);
+  return(stringArr.reduceRight((accumulator, currentValue) => accumulator + currentValue));
+}
+
+module.exports = {capitalize, reverseString};
