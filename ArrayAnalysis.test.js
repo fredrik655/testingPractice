eae8d86e442a6c3d49e8b92c7defe2f185bfac12
anon <anon@example.com>
2021-01-01 15:49:35 +0100

    Updated test cases to expect a return object

--- ArrayAnalysis.test.js
+++ ArrayAnalysis.test.js
@@ -9,31 +9,31 @@ structureObject = {
   length: expect.any(Number)
 };
 
-test.only('check if structure is correct', () => {
+test('check if structure is correct', () => {
   expect(analyze([1,8,3,4,2,6])).toMatchObject(structureObject);
   expect(analyze([1,6])).toMatchObject(structureObject);
 });
 
-test('average of all numbers', () => {
-  expect(analyze([1,8,3,4,2,6])).toBe(4);
-  expect(analyze([1,6])).toBeCloseTo(3.5);
-  expect(analyze([1,-8,2,6])).toBeCloseTo(0.25);
+test.only('average of all numbers', () => {
+  expect(analyze([1,8,3,4,2,6]).average).toBe(4);
+  expect(analyze([1,6]).average).toBeCloseTo(3.5);
+  expect(analyze([1,-8,2,6]).average).toBeCloseTo(0.25);
 });
 
 test('min number', () => {
-  expect(analyze([1,8,3,4,2,6])).toBe(1);
-  expect(analyze([1,-2,6])).toBe(-2);
-  expect(analyze([3,88,2,6])).toBe(2);
+  expect(analyze([1,8,3,4,2,6]).min).toBe(1);
+  expect(analyze([1,-2,6]).min).toBe(-2);
+  expect(analyze([3,88,2,6]).min).toBe(2);
 });
 
 test('max number', () => {
-  expect(analyze([1,8,3,4,2,6])).toBe(8);
-  expect(analyze([1,-2,6])).toBe(6);
-  expect(analyze([3,88,2,6])).toBe(88);
+  expect(analyze([1,8,3,4,2,6]).max).toBe(8);
+  expect(analyze([1,-2,6]).max).toBe(6);
+  expect(analyze([3,88,2,6]).max).toBe(88);
 });
 
 test('length of the object', () => {
-  expect(analyze([1,8,3,4,2,6])).toBe(6);
-  expect(analyze([1,-2,6])).toBe(3);
-  expect(analyze([3,88,2,6])).toBe(4);
+  expect(analyze([1,8,3,4,2,6]).length).toBe(6);
+  expect(analyze([1,-2,6]).length).toBe(3);
+  expect(analyze([3,88,2,6]).length).toBe(4);
 });
