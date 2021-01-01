e3f2611667b92a1db69e08ae1313f13c7241b4b8
anon <anon@example.com>
2021-01-01 11:31:58 +0100

    implement test case for calculator add func

--- index.test.js
+++ index.test.js
@@ -1,7 +1,15 @@
 const { test, expect } = require("@jest/globals");
 const indexModule = require('./index');
+const calculatorModule = require('./Calculator');
+
 const capitalize = indexModule.capitalize;
 const reverseString = indexModule.reverseString;
+
+const add = calculatorModule.add;
+const subtract = calculatorModule.subtract;
+const divide = calculatorModule.divide;
+const multiply = calculatorModule.multiply;
+
 
 
 test('capitalize first letter of string ', () => {
@@ -9,10 +17,26 @@ test('capitalize first letter of string ', () => {
   expect(capitalize('abc')).toBe('Abc');
   expect(capitalize('capitalize')).toBe('Capitalize');
   expect(capitalize('hello WorLd')).toBe('Hello world');
-})
+});
 
 test('Reverses a string', () => {
   expect(reverseString('Hello World')).toBe('dlroW olleH');
   expect(reverseString('Abcd')).toBe('dcbA');
   expect(reverseString('Anna')).toBe('annA');
+});
+
+test('add whole number', () => {
+  expect(add(1, 3)).toBe(4);
+  expect(add(2, 2)).toBe(4);
 })
+
+test('add neg number', () => {
+  expect(add(-1, 3)).toBe(2);
+  expect(add(-2, -2)).toBe(-4);
+})
+
+test('add rational number', () => {
+  expect(add(0.5, 3.5)).toBe(4);
+  expect(add(2, 7.2)).toBe(9.2);
+})
+
